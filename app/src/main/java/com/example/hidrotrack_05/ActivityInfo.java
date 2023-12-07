352a8c9f9b428b223099ab0cbddc5505e35b1fd9
davjuv <davinnci06@gmail.com>
2023-12-06 20:26:47 -0500

    Version inicial

---
+++ app/src/main/java/com/example/hidrotrack_05/ActivityInfo.java
@@ -0,0 +1,14 @@
+package com.example.hidrotrack_05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
---
+++ app/src/main/java/com/example/hidrotrack_05/ActivityProgress.java
@@ -0,0 +1,14 @@
+package com.example.hidrotrack_05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityProgress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress);
+    }
+}
